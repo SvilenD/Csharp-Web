c298e7051a26522176aa8fa8c9ad27ae6ecf432a
SvilenD <42715140+SvilenD@users.noreply.github.com>
2020-01-24 11:05:43 +0200

    testing

---
+++ HttpTestServer/HttpTestServer/ClientProcessor.cs
@@ -0,0 +1,56 @@
+﻿namespace HttpTestServer
+{
+    using System;
+    using System.Text;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+
+    public class ClientProcessor
+    {
+        private const string NewLine = "\r\n";
+        private readonly TcpClient client;
+
+        public ClientProcessor(TcpClient client)
+        {
+            this.client = client;
+            Task.Run(() => this.ProcessClientAsync());
+        }
+
+        private async Task ProcessClientAsync()
+        {
+            using (NetworkStream networkStream = client.GetStream())
+            {
+                StringBuilder requestInfo = new StringBuilder();
+
+                while (networkStream.DataAvailable && networkStream.CanRead)
+                {
+                    byte[] requestBytes = new byte[256];
+
+                    int readBytes = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                    requestInfo.Append(Encoding.UTF8.GetString(requestBytes, 0, readBytes));
+                }
+
+                Console.WriteLine(new string('=', 70));
+                Console.WriteLine(requestInfo);
+
+                string responseBody = @"<form action='/Account/Login' method='post'>
+                                            <label for='username'> Username: </label>
+                                            <input type=text name='username' />
+                                            <label for='password'>Password: </label>
+                                            <input type=password name='pasword' />
+                                            <input type=date name='date' />
+                                            <input type=submit value='Login' />
+                                            </form>";
+                string response = "HTTP/1.1 200 OK" + NewLine + //"HTTP/1.1 301 Moved" + NewLine +
+                                  "Content-Type: text/html" + NewLine +
+                                  //"Location: https://google.com" + NewLine +
+                                  //"Content-Disposition: attachment; filename=index.html" + NewLine +
+                                  "Server: MyCustomServer/1.0" + NewLine +
+                                  $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
+                                  responseBody;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            }
+        }
+    }
+}
---
+++ HttpTestServer/HttpTestServer/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace HttpTestServer
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Sockets;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        const int Port = 12345;
+
+        public static async Task Main()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, Port);
+            tcpListener.Start();
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            Task.Run(() => HttpRequesterAsync());
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            // WebUtility.UrlDecode
+            while (true)
+            {
+                // TCP / UDP
+                TcpClient client = await tcpListener.AcceptTcpClientAsync();
+                var clientProcessor = new ClientProcessor(client);
+            }
+        }
+
+        public async static Task HttpRequesterAsync()
+        {
+            Console.WriteLine("--------------------START OF HTTP REQUESTER JOB!--------------------");
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://softuni.bg");
+            string result = await response.Content.ReadAsStringAsync();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            File.WriteAllTextAsync("../../../result.html", result);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            
+            Thread.Sleep(10000);
+
+            Console.WriteLine(response);
+            Console.WriteLine("---------------------END OF HTTP REQUESTER JOB!---------------------");
+        }
+    }
+}
